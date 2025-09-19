6816d27502efcb7cecf00644b7f6dd74e702a8bd
pragadeeshm-dotcom <pragadeesh.m@intellicredence.com>
2025-09-19 11:44:37 +0530

    RestAssureAPI

--- src/test/java/RestAssureTest/Sample.java
+++ src/test/java/RestAssureTest/Sample.java
@@ -1,7 +1,27 @@
 package RestAssureTest;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
 public class Sample {
 	
+	@Test
+	void getUser() {
+
+		given()
+
+			.when()
+
+				.get("https://reqres.in/api/users?page=2")
+
+			.then()
+
+				.statusCode(200).body("page", equalTo(2)).log().all();
+
+	}
+
 	
 	
 
